2becf0113944de31d4a10e7650a442f6e2e45c8a
Amara Ramm (RIT Student) <aer5363@rit.edu>
2026-04-06 11:38:27 -0400

    worked on question 2, its done good enough. doesnt work tho

--- src/main/java/mte3/knockknock/KnockKnockClient.java
+++ src/main/java/mte3/knockknock/KnockKnockClient.java
@@ -3,8 +3,11 @@
 
 package mte3.knockknock;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 import java.util.Scanner;
@@ -14,24 +17,38 @@ public class KnockKnockClient {
     public static String SERVER = "localhost";
 
     public static void sendAndReceive(PrintWriter writer, String message, Scanner scanner) {
-        
-        // 
-        // 
-        // 
-
+        writer.println(message);
+        System.out.println("Sent:" + message);
     } // sendAndReceive() method closed
     
 
     public static void joke(String who,String punchLine) throws IOException {
-        
-        //  
-        // 
-        // 
+        try (Socket socket = new Socket(SERVER, PORT);
+            Scanner scan = new Scanner(System.in)
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        ) {
+            sendAndReceive(writer, "Knock, Knock", scan);
+            receiveAndSend(scan, "Who's there?", writer, true);
+        
+            sendAndReceive(writer, who, scan);
+            receiveAndSend(scan, who + ", Who?", writer, true);
+        
+            sendAndReceive(writer, punchLine, scan);
+            receiveAndSend(scan, "Haha, nice one!", writer, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        ss.close();
+        socket.close();
+        in.close();
+        writer.close();
     } // joke() method closed
 
     public static void main(String[] args) throws IOException {
-
         String[][] jokes = {{"Tank","You're welcome!"},
                             {"Nobel","Nobel...that's why I knocked!"},
                             {"Says","Says me!"},
@@ -42,7 +59,7 @@ public class KnockKnockClient {
                         };
         int i = new Random().nextInt(jokes.length);
         joke(jokes[i][0],jokes[i][1]);
-
+        
     } // main() method closed
 
     // hint: please run KnockKnockServer first and then run KnockKnockClient
--- src/main/java/mte3/knockknock/KnockKnockServer.java
+++ src/main/java/mte3/knockknock/KnockKnockServer.java
@@ -3,7 +3,9 @@
 
 package mte3.knockknock;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -13,19 +15,36 @@ import java.util.Scanner;
 public class KnockKnockServer {
     public static int PORT = 54322;
 
-    public static void receiveAndSend(Scanner scanner,String message,PrintWriter writer,boolean concat) {
-        
-        // 
-        // 
-        // 
+    public static void receiveAndSend(Scanner scanner, String message, PrintWriter writer, boolean concat) {
+        String msg = scanner.nextLine();
+        System.out.println("Recieved: " + msg); 
 
     } // receiveAndSend() method closed
     
     public static void main(String args[]) throws IOException {
+        try (ServerSocket ss = new ServerSocket(PORT);
+            Socket socket = ss.accept();
+            Scanner scan = new Scanner(System.in)
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         
-        // 
-        // 
-        // 
-    
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        ) {
+            msg1 = sendAndReceive(writer, "Knock, Knock", scan);
+            receiveAndSend(scan, "Who's there?", writer, true);
+        
+            msg2 = sendAndReceive(writer, who, scan);
+            receiveAndSend(scan, who + ", Who?", writer, true);
+        
+            msg3 = sendAndReceive(writer, punchLine, scan);
+            receiveAndSend(scan, "Haha, nice one!", writer, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ss.close();
+        socket.close();
+        in.close();
+        writer.close();
     } // main() method closed
 }
